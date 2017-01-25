ac4c30eddc3216181a53eb949b3b3fa7c6441d1d
Nick Hudak <nhudak@pentaho.com>
2017-01-25 14:40:13 -0500

    remote features via RSA

--- pdi-execution-engine/impl/remote/impl/src/main/java/org/pentaho/di/engine/remote/ExecutionManager.java
+++ pdi-execution-engine/impl/remote/impl/src/main/java/org/pentaho/di/engine/remote/ExecutionManager.java
@@ -1,10 +1,16 @@
 package org.pentaho.di.engine.remote;
 
+import com.google.common.base.Preconditions;
 import org.pentaho.di.engine.api.ITransformation;
 import org.pentaho.di.engine.api.remote.IExecutionManager;
+import rx.Observable;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
 
 /**
  * Created by hudak on 1/20/17.
