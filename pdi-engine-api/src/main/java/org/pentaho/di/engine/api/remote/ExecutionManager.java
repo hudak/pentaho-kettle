e248c7e5bb24e1c73739104e5c07770d9494cfdc
Nick Hudak <nhudak@pentaho.com>
2017-02-15 20:57:42 -0500

    [BACKLOG-14633]Remove Streams & Futures from remote api
    Simplified remote API with only serializable types. Clients must connect
    to a server to stream events

--- pdi-engine-api/src/main/java/org/pentaho/di/engine/api/remote/ExecutionManager.java
+++ pdi-engine-api/src/main/java/org/pentaho/di/engine/api/remote/ExecutionManager.java
@@ -1,15 +1,16 @@
 package org.pentaho.di.engine.api.remote;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.URI;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * A service (usually running on a remote daemon) that can accept Execution Requests
  * <p>
  * Created by hudak on 2/7/17.
  */
-public interface ExecutionManager extends Serializable {
+public interface ExecutionManager {
   /**
    * @return common name this service's backing engine, e.g. "Spark"
    */
@@ -23,16 +24,17 @@ public interface ExecutionManager extends Serializable {
   /**
    * @return environment of backing engine
    */
-  Map<String, Object> getEnvironment();
+  Map<String, Serializable> getEnvironment();
 
   /**
    * Submit a transformation to this execution manager.
    * <p>
-   * If submission was successful, a unique service property will be returned, which can then be used to locate
-   * the {@link Execution} created from this request.
+   * If submission was successful, a server socket address will be returned, which can then be used to
+   * stream events
    *
    * @param request {@link ExecutionRequest}
-   * @return endpointId used to locate an {@link Execution}
+   * @return URI with the format tcp://$host:$port
+   * @throws IOException Request could not be submitted event stream server could not start
    */
-  CompletableFuture<String> submit( ExecutionRequest request );
+  URI submit( ExecutionRequest request ) throws IOException;
 }
